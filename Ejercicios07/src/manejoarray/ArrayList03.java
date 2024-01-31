f7c9b4f111d4d33e22291326cde3c3b826e821d6
erick <estoledo1@utpl.edu.ec>
2024-01-31 15:53:19 -0500

    ArrayList

--- Ejercicios07/src/manejoarray/ArrayList03.java
+++ Ejercicios07/src/manejoarray/ArrayList03.java
@@ -13,41 +13,9 @@ import java.util.Scanner;
  *
  * @author reroes
  */
-public class ArrayList01 {
+public class ArrayList03 {
 
     public static void main(String[] args) {
-
-        // creación de un ArrayList
-        ArrayList<Integer> arreglo = new ArrayList<>();
-
-        arreglo.add(10);
-        arreglo.add(20);
-        arreglo.add(30);
-        arreglo.add(40);
-        arreglo.add(50);
-        arreglo.add(60);
-        arreglo.add(1000);
-        arreglo.add(2000);
-        arreglo.add(3000);
-
-        // for (int i = 0; i < arreglo.size(); i++) {
-        //    System.out.println(arreglo.get(i));
-        // }
-
-        System.out.println("----------------------------------");
-
-        ArrayList<Double> arreglo3 = new ArrayList<>();
-
-        arreglo3.add(10.2);
-        arreglo3.add(11.2);
-        arreglo3.add(12.2);
-        arreglo3.add(13.2);
-
-        // for (int i = 0; i < arreglo3.size(); i++) {
-        //    System.out.println(arreglo3.get(i));
-        // }
-
-        System.out.println("----------------------------------");
         
         ArrayList<String> arreglo2 = new ArrayList<>();
         boolean bandera = true;
@@ -76,6 +44,6 @@ public class ArrayList01 {
         ArrayList <String> arreglo2 = new ArrayList<>();
         
         ArrayList <Boolean> arreglo4 = new ArrayList<>();
-         */
+      /*   */
     }
 }
